acf84bd9df3e0eef2740bcf4d8642018a844f958
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-30 02:55:25 +0000

    feat: Add the grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,26 @@
 	<title>Testimonials Grid Section Challenge</title>
 </svelte:head>
 
-<div class="flex h-screen items-center justify-center text-5xl font-semibold">
-	Testimonials Grid Section Challenge
+<div
+	class="flex min-h-screen items-center justify-center text-center align-middle text-5xl font-semibold text-white"
+>
+	<div class="grid max-w-6xl grid-cols-1 gap-6 md:grid-cols-4 md:grid-rows-2 md:gap-8">
+		<div class="h-full w-full bg-red-500 md:order-1 md:col-span-2">
+			<div class="flex h-full items-center justify-center p-32">1</div>
+		</div>
+		<div class="h-full w-full bg-yellow-500 md:order-2">
+			<div class="flex h-full items-center justify-center p-32">2</div>
+		</div>
+		<div class="h-full w-full bg-purple-500 md:order-4">
+			<div class="flex h-full items-center justify-center p-32">3</div>
+		</div>
+		<div class="h-full w-full bg-gray-500 md:order-5 md:col-span-2">
+			<div class="flex h-full items-center justify-center p-32">4</div>
+		</div>
+		<div class="h-full w-full bg-orange-600 md:order-3 md:row-span-2">
+			<div class="flex h-full items-center justify-center p-32">5</div>
+		</div>
+	</div>
 </div>
 
 <!-- Daniel Clifford -->
