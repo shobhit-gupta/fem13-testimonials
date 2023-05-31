adb94d71858db652e09f304caa219c3ed715ce17
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-31 03:41:50 +0000

    refactor: Abstract out cards component & data

---
+++ src/lib/UI/Card.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let name: string;
+	export let imgSrc: string;
+</script>
+
+<!-- Card -->
+<div class="card {$$restProps.class || ''}">
+	<div class="flex items-start gap-4">
+		<div class="card__pic-wrapper">
+			<img src={imgSrc} alt="Profile pic of {name}" />
+		</div>
+		<div>
+			<h2 class="leading-none">{name}</h2>
+			<p class="mt-1 text-xs leading-none opacity-50">Verified Graduate</p>
+		</div>
+	</div>
+
+	<p class="text-xl font-semibold leading-6">
+		<slot name="headline">Headline</slot>
+	</p>
+
+	<p class="opacity-70">
+		<slot name="text">Text</slot>
+	</p>
+</div>
+
+<style lang="postcss">
+	:global(.card) {
+		@apply m-6 max-w-xs space-y-4 rounded-lg p-8 pt-7 shadow-lg shadow-veryDarkGrayishBlue/25;
+	}
+	:global(.card__pic-wrapper) {
+		@apply h-7 w-7 overflow-hidden rounded-full border-2 border-white border-opacity-20;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,47 @@
+<script lang="ts">
+	import Card from '$lib/UI/Card.svelte';
+
+	const testimonials = [
+		{
+			userPic: '/images/image-daniel.jpg',
+			userName: 'Daniel Clifford',
+			headline:
+				'I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.',
+			text: '“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experince here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”',
+			styles: 'bg-moderateViolet text-white'
+		},
+		{
+			userPic: '/images/image-jonathan.jpg',
+			userName: 'Jonathan Walters',
+			headline: 'The team was very supportive and kept me motivated',
+			text: '“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. ”',
+			styles: 'bg-veryDarkGrayishBlue text-white'
+		},
+		{
+			userPic: '/images/image-jeanette.jpg',
+			userName: 'Jeanette Harmon',
+			headline: 'An overall wonderful and rewarding experience',
+			text: '“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”',
+			styles: 'bg-white text-veryDarkGrayishBlue'
+		},
+		{
+			userPic: '/images/image-patrick.jpg',
+			userName: 'Patrick Abrams',
+			headline:
+				'Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.',
+			text: '“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”',
+			styles: 'bg-veryDarkBlackishBlue text-white'
+		},
+		{
+			userPic: '/images/image-kira.jpg',
+			userName: 'Kira Whittle',
+			headline: 'Such a life-changing experience. Highly recommended!',
+			text: '“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend! ”',
+			styles: 'bg-white text-veryDarkGrayishBlue'
+		}
+	];
+</script>
+
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
@@ -30,75 +74,25 @@
 	</div>
 </div> -->
 
-<div class="flex min-h-screen items-center justify-center text-white">
-	<div
-		class="m-6 max-w-lg space-y-4 rounded-lg bg-moderateViolet p-8 pt-7 shadow-lg shadow-veryDarkGrayishBlue/25"
-	>
-		<div class="flex items-start gap-4">
-			<div
-				class="h-7 w-7 overflow-hidden rounded-full border-2 border-white border-opacity-20"
-			>
-				<img src="/images/image-daniel.jpg" alt="" />
-			</div>
-			<div>
-				<h2 class="leading-none">Daniel Clifford</h2>
-				<p class="mt-1 text-xs leading-none opacity-50">Verified Graduate</p>
-			</div>
-		</div>
-
-		<p class="text-xl font-semibold leading-6">
-			I received a job offer mid-course, and the subjects I learned were current, if not more
-			so, in the company I joined. I honestly feel I got every penny’s worth.
-		</p>
-
-		<p class="opacity-50">
-			“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
-			transition and have heard some people who had an amazing experience here. I signed up
-			for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
-			The next 12 weeks was the best - and most grueling - time of my life. Since completing
-			the course, I’ve successfully switched careers, working as a Software Engineer at a VR
-			startup. ”
-		</p>
-	</div>
+<!-- Centered container -->
+<div
+	class="
+        flex
+        min-h-screen
+        items-center
+        justify-center
+    "
+>
+	{#each testimonials as { styles, userPic: imgSrc, userName: name, headline, text }}
+		<Card class={styles} {imgSrc} {name}>
+			<svelte:fragment slot="headline">{headline}</svelte:fragment>
+			<svelte:fragment slot="text">{text}</svelte:fragment>
+		</Card>
+	{/each}
 </div>
 
-<!-- Jonathan Walters -->
-<!-- Verified Graduate -->
-
-<!-- The team was very supportive and kept me motivated -->
-
-<!-- “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer  -->
-<!-- for a big company. This was one of the best investments I’ve made in myself. ” -->
-
-<!-- Jeanette Harmon -->
-<!-- Verified Graduate -->
-
-<!-- An overall wonderful and rewarding experience -->
-
-<!-- “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living  -->
-<!-- while doing something I love. ” -->
-
-<!-- Patrick Abrams -->
-<!-- Verified Graduate -->
-
-<!-- Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and  -->
-<!-- learning from their experiences was easy. -->
-
-<!-- “ The staff seem genuinely concerned about my progress which I find really refreshing. The program  -->
-<!-- gave me the confidence necessary to be able to go out in the world and present myself as a capable  -->
-<!-- junior developer. The standard is above the rest. You will get the personal attention you need from  -->
-<!-- an incredible community of smart and amazing people. ” -->
-
-<!-- Kira Whittle -->
-<!-- Verified Graduate -->
-
-<!-- Such a life-changing experience. Highly recommended! -->
-
-<!-- “ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from  -->
-<!-- professionals who can help me learn programming step by step. I was encouraged to enroll by a former  -->
-<!-- student of theirs who can only say wonderful things about the program. The entire curriculum and staff  -->
-<!-- did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team  -->
-<!-- project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial  -->
-<!-- could ever have. In fact, I’ve often referred to it during interviews as an example of my developent  -->
-<!-- experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers.  -->
-<!-- 100% recommend! ” -->
+<style lang="postcss">
+	:global(.card:first-of-type) {
+		@apply bg-[url('/images/bg-pattern-quotation.svg')] bg-[position:_right_1.5rem_top] bg-no-repeat md:bg-[position:_right_5rem_top];
+	}
+</style>
